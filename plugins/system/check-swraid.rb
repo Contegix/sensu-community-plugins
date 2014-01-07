0f322dc517622170289ba0422ca18d441ae0919b
Christopher Geers <christopher.geers@contegix.com>
2014-01-06 19:31:17 -0600

    critical conditions are observed

--- plugins/system/check-swraid.rb
+++ plugins/system/check-swraid.rb
@@ -20,10 +20,25 @@
 require 'sensu-plugin/check/cli'
 
 class SWRAIDStatus < Sensu::Plugin::Check::CLI
+  CRITICAL_EVENTS = ['DegradedArray', 'DeviceDisappeared', 'Fail', 'FailSpare']
+  WARNING_EVENTS = ['SparesMissing']
   def run
-    unknown
-    critical
+    if File.executable? '/sbin/mdadm'
+      test_stdout = %x{/sbin/mdadm --detail --scan --test}
+      unknown message "error scanning for software RAID devices" if $?.exitstatus == 4
+      unknown message "no software RAID devices found" if test_stdout.lines.count == 0
+      ok if $?.exitstatus == 0
+    else
+      unknown message "software RAID is not available"
+    end
+    # There must be a problem
+    problem_arrays = []
+    array_details = "\n"
+    %x{/sbin/mdadm --monitor --oneshot --scan --program=/bin/echo}.each_line do |line|
+      problem_arrays << line.split(' ')[1] if CRITICAL_EVENTS.include? line.split(' ')[0]
+    end
+    problem_arrays.each { |array| array_details << %x{/sbin/mdadm --detail #{array}} }
+    critical array_details
     warning
-    ok
   end
 end
